f3677bca8e93474c1c4c3d02f1493705d09a1990
Bulbul-Programing <bulbul.programing@gmail.com>
2025-10-09 00:32:44 +0600

    Navbar and footer add .

--- src/components/theme-toggle.tsx
+++ src/components/theme-toggle.tsx
@@ -1,12 +1,20 @@
-'use client'
-import { useState } from "react"
+"use client"
+
 import { MoonIcon, SunIcon } from "lucide-react"
 
 import { Toggle } from "@/components/ui/toggle"
+import { useTheme } from "next-themes";
+import { useEffect, useState } from "react";
 
 export default function ThemeToggle() {
-  const [theme, setTheme] = useState<string>("device")
+  const { theme, setTheme, systemTheme } = useTheme();
+  const [mounted, setMounted] = useState(false);
 
+  useEffect(() => setMounted(true), []);
+  if (!mounted) return null;
+
+  const currentTheme = theme === "system" ? systemTheme : theme;
+  
   return (
     <div>
       <Toggle
@@ -14,11 +22,11 @@ export default function ThemeToggle() {
         className="group data-[state=on]:hover:bg-muted text-muted-foreground data-[state=on]:text-muted-foreground data-[state=on]:hover:text-foreground size-8 rounded-full border-none shadow-none data-[state=on]:bg-transparent"
         pressed={theme === "dark"}
         onPressedChange={() =>
-          setTheme(theme === "dark" ? "light" : "dark")
+          setTheme(currentTheme === "dark" ? "light" : "dark")
         }
-        aria-label={`Switch to ${theme === "dark" ? "light" : "dark"} mode`}
+        aria-label={`Switch to ${currentTheme === "dark" ? "light" : "dark"} mode`}
       >
-
+        {/* Note: After dark mode implementation, rely on dark: prefix rather than group-data-[state=on]: */}
         <MoonIcon
           size={16}
           className="shrink-0 scale-0 opacity-0 transition-all group-data-[state=on]:scale-100 group-data-[state=on]:opacity-100"
