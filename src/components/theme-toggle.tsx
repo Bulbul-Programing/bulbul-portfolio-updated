edea618b2a525d0e93963611d9476f3a23212438
Bulbul-Programing <bulbul.programing@gmail.com>
2025-10-09 13:28:00 +0600

    Project start:

--- src/components/theme-toggle.tsx
+++ src/components/theme-toggle.tsx
@@ -10,7 +10,7 @@ export default function ThemeToggle() {
   const { theme, setTheme, systemTheme } = useTheme();
   const [mounted, setMounted] = useState(false);
 
-  useEffect(() => setMounted(true), [])
+  useEffect(() => setMounted(true), []) 
   if (!mounted) return null;
 
   const currentTheme = theme === "system" ? systemTheme : theme;
